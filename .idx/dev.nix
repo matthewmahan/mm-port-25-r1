56a235acd67822e276480427f1aab250e9557485
mahan.mattr <mahan.mattr@gmail.com>
2025-03-29 20:58:25 +0000

    Project setup

---
+++ .idx/dev.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }: {
+
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_18
+  ];
+
+  # Sets environment variables in the workspace
+  env = {
+    SOME_ENV_VAR = "hello";
+  };
+
+  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+  idx.extensions = [
+    "angular.ng-template"
+    "astro-build.astro-vscode"
+    "bradlc.vscode-tailwindcss"
+  ];
+
+  # Enable previews and customize configuration
+  idx.previews = {
+    enable = true;
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "start"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+          "--disable-host-check"
+        ];
+        manager = "web";
+        # Optionally, specify a directory that contains your web app
+        # cwd = "app/client";
+      };
+    };
+  };
+}
